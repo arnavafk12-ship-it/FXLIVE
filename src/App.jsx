64a3d9e65d724a0b294c9ce7d9071b0b1ccd087d
Arnav_Singh <arnavafk12@gmail.com>
2026-08-09 13:18:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef, useCallback, useMemo } from "react";
+import React, { useState, useEffect, useRef, useCallback, useMemo } from "react";
 import {
   ResponsiveContainer,
   LineChart,
